abe67a0f804461446b129c8496b95898312032c4
Michelle <nyamburamichelle400@gmail.com>
2024-04-24 08:58:52 +0300

    completes lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const firebrick ={
+    color:"firebrick",
+  }
+  return(
+   <div id="home">
+    <h1 style={firebrick}> {name} is a Web Developer from {city}</h1>
+   </div>
+   )
 }
 
 export default Home;
